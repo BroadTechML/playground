a994e65a0352ce2ab455f5ece0cbf976418c0d96
shuimei <dengshunqiangWoutlook.com>
2018-12-07 13:33:36 +0800

    modified missing count, add return plot

--- Jinghui/help_functions.py
+++ Jinghui/help_functions.py
@@ -16,4 +16,4 @@ def plot_miss_count(df, figsize, title):
     miss_val_pct = 100 * miss_val_count / df.shape[0]
     miss_val_pct = pd.DataFrame(miss_val_pct)
     miss_val_pct = miss_val_pct.rename(columns={0:'percent'})
-    miss_val_pct.sort_values(by='percent').plot(kind='barh', figsize=figsize, title=title)
+    return miss_val_pct.sort_values(by='percent').plot(kind='barh', figsize=figsize, title=title)
