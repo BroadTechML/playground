ca51f1175173b7d384fcbb536233546dfe2d627f
shuimei <dengshunqiangWoutlook.com>
2018-12-10 14:54:54 +0800

    add return:miss_val_pct, add optioanl params:figsize, title

--- Jinghui/help_functions.py
+++ Jinghui/help_functions.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 # 缺失值统计
-def plot_miss_count(df, figsize, title):
+def plot_miss_count(df, figsize=(10, 12), title="missing counts"):
     '''
     df : DataFrame
     figsize : tuple(x, y)
@@ -16,4 +16,4 @@ def plot_miss_count(df, figsize, title):
     miss_val_pct = 100 * miss_val_count / df.shape[0]
     miss_val_pct = pd.DataFrame(miss_val_pct)
     miss_val_pct = miss_val_pct.rename(columns={0:'percent'})
-    return miss_val_pct.sort_values(by='percent').plot(kind='barh', figsize=figsize, title=title)
+    return miss_val_pct, miss_val_pct.sort_values(by='percent').plot(kind='barh', figsize=figsize, title=title)
